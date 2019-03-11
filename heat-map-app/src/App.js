bd2dac05279cb4fb827c136278d24dd66585a24a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-11 14:43:42 -0400

    Created <LoginForm /> component for App.js

--- heat-map-app/src/App.js
+++ heat-map-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import LoginForm from "./components/LoginForm/LoginForm";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Heat Map App!
+        <LoginForm />
       </div>
     );
   }

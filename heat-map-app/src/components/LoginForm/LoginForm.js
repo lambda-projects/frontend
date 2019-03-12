1bde00a5e19ebf0d1e43e6884ad06491162e811b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-12 12:05:46 -0400

    Updated <input> placeholder on <form> from 'Email Address' to 'Username'

--- heat-map-app/src/components/LoginForm/LoginForm.js
+++ heat-map-app/src/components/LoginForm/LoginForm.js
@@ -33,7 +33,7 @@ const LoginForm = () => (
 				</Header>
 				<Form size='large'>
 					<Segment stacked>
-						<Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+						<Form.Input fluid icon='user' iconPosition='left' placeholder='Username' />
 						<Form.Input
 						fluid
 						icon='lock'

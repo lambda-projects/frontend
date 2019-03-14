6b0303eb0f6c2dba59c12fa78aae18eac9a562e2
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-14 17:54:28 -0400

    Added another scatter plot for Shreyas

--- heat-map-app/src/components/Embed/Embed.js
+++ heat-map-app/src/components/Embed/Embed.js
@@ -4,7 +4,7 @@ class Embed extends React.Component {
   
   render() {
     return (
-        <iframe src="http://162.213.248.227:9150" width="700" height="600"></iframe>
+        <iframe src="http://162.213.248.227:9150" width="700" height="600" title="2013 US Gun Violence"></iframe>
     )
   }
 }

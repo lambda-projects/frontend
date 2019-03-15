c8f9200c5a562986f791909ae1f932a54574be33
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-14 21:24:04 -0400

    Modified article cards for Daniel's articles

--- heat-map-app/src/components/Articles/Articles.js
+++ heat-map-app/src/components/Articles/Articles.js
@@ -7,11 +7,29 @@ const Articles = () => (
   <Card.Group className='articles-container'>
     <Card>
       <Card.Content>
-        <Image floated='right' size='mini' src='/images/avatar/large/steve.jpg' />
-        <Card.Header>Steve Sanders</Card.Header>
-        <Card.Meta>Friends of Elliot</Card.Meta>
+        <Card.Header>Top 20 States for Gun Violence Per Capita</Card.Header>
+        <Card.Meta>Written by Daniel Harris</Card.Meta>
         <Card.Description>
-          Steve wants to add you to the group <strong>best friends</strong>
+            <Image floated='right' size='mini' src='/images/avatar/large/steve.jpg' />
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green'>
+            View
+          </Button>
+          <Button basic color='red'>
+            Close
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
+    <Card>
+      <Card.Content>
+        <Card.Header>Mass shootings, by location</Card.Header>
+        <Card.Meta>Written by Daniel Harris</Card.Meta>
+        <Card.Description>
+            <Image floated='right' size='mini' src='/images/avatar/large/molly.png' />
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
@@ -27,30 +45,11 @@ const Articles = () => (
     </Card>
     <Card>
       <Card.Content>
-        <Image floated='right' size='mini' src='/images/avatar/large/molly.png' />
-        <Card.Header>Molly Thomas</Card.Header>
-        <Card.Meta>New User</Card.Meta>
+        <Card.Header>Why has Gun Violence Increased in Texas?</Card.Header>
+        <Card.Meta>Written by Daniel Harris</Card.Meta>
         <Card.Description>
-          Molly wants to add you to the group <strong>musicians</strong>
+            <Image floated='right' size='mini' src='/images/avatar/large/jenny.jpg' />
         </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='green'>
-            Approve
-          </Button>
-          <Button basic color='red'>
-            Decline
-          </Button>
-        </div>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Card.Content>
-        <Image floated='right' size='mini' src='/images/avatar/large/jenny.jpg' />
-        <Card.Header>Jenny Lawrence</Card.Header>
-        <Card.Meta>New User</Card.Meta>
-        <Card.Description>Jenny requested permission to view your contact details</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <div className='ui two buttons'>
